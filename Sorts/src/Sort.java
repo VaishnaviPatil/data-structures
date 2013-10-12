9ee33a7532df70f7700e6ef3ae578aa22bce5acf
VaishnaviPatil <vaishnap@usc.edu>
2013-10-12 16:36:21 -0700

    practice

--- Sorts/src/Sort.java
+++ Sorts/src/Sort.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 
 public class Sort {
 	
@@ -19,6 +21,8 @@ public class Sort {
 		int arr[] = new int[high-low+1], mid = (low+high)/2;
 		
 		int i = low, j = mid+1, k = 0;
+		
+		//walk through both halves of the array
 		while(i <= mid && j<=high) {
 			if(array[i] > array[j])
 				arr[k++] = array[j++];
@@ -68,7 +72,7 @@ public class Sort {
 				array[j+1] = array[j];
 				j--;
 			}
-			if(j>=0 && array[j+1] > current)
+			if(array[j+1] > current)
 				array[j+1] = current;
 			
 			DisplayArray(array);
@@ -79,7 +83,8 @@ public class Sort {
 		int i, j, min;
 		
 		for(i=0; i<array.length-1; i++) {
-			min = i;
+			//find minimum
+			min = i;			
 			for(j=i+1; j<array.length; j++ ) {
 				if(array[min] > array[j])
 					min = j;
@@ -92,13 +97,16 @@ public class Sort {
 	
 	int Partition(int array[], int low, int high) {		
 		int i, k=low, pivot = array[low];
+		
+		//put pivot at the end
 		Swap(low, high, array); 
 		
-		
 		for(i=low; i<high; i++)
+			//put lower elements to the left 
 			if(array[i] < pivot)
 				Swap(k++, i, array);
 		
+		//get pivot to it's correct position
 		Swap(k, high, array);
 		return k;
 	}
@@ -112,19 +120,70 @@ public class Sort {
 			DisplayArray(array);
 		}		
 	}
+	
+	int FindDigits(int number) {
+		int digits = 0;
+		while(number>0) {
+			number /= 10;
+			digits++;
+		}
+		return digits;
+	}
+	
+	void RadixSort(int array[]) {
+		int digits, i, j, k, ind, max = 0;
+		ArrayList<ArrayList<Integer>> buckets = new ArrayList<ArrayList<Integer>>();
+		
+		//find number of max digits
+		for(i=0; i<array.length; i++) {
+			digits = FindDigits(array[i]);
+			max = max<digits? digits:max ;
+		}
+		digits = max;
+		
+		System.out.println("digits = "+digits);
+		int div = 1;
+
+		//initialize buckets
+		for(i=0; i<10; i++) {
+			ArrayList<Integer> bucket = new ArrayList<Integer>();
+			buckets.add(bucket);
+		}
+		
+		for(i=0; i<digits; i++) {
+			//clear buckets for each pass 
+			for(j=0; j<10; j++) {			
+				buckets.get(j).clear();
+			}
+
+			//add elements to correct bucket
+			for(j=0; j<array.length; j++) 
+				buckets.get((array[j]/(div))%10).add(array[j]);
+			
+			//sort array
+			ind = 0;
+			for(j=0; j<buckets.size(); j++) {
+				for(k=0; k<buckets.get(j).size(); k++)
+					array[ind++] = buckets.get(j).get(k);
+			}			
+			div = div*10;
+			DisplayArray(array);
+		}
+	}
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		int arr[] = {1,4,7,2,3,8,5};		
+		int arr[] = {101,45,7,2, 44, 8, 5, 11};		
 		Sort sort = new Sort();
-		//sort.BubbleSort(arr);
+		sort.BubbleSort(arr);
 		//sort.MergeSort(arr, 0, arr.length-1);
 		//sort.InsertionSort(arr);
 		//sort.SelectionSort(arr);
-		sort.QuickSort(arr, 0, arr.length-1);
+		//sort.QuickSort(arr, 0, arr.length-1);
+		//sort.RadixSort(arr);
 	}
 }
 
